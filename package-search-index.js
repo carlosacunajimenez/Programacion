d5a5c7c941045ff571c572263b8f9045df39fbf3
carlosacunajimenez <carloswizyo@hotmail.com>
2022-02-07 14:16:23 +0100

    pagina xd

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"entornozzz"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"entornozzz","l":"circulo"},{"p":"entornozzz","l":"Empleado"},{"p":"entornozzz","l":"Javadoc"},{"p":"entornozzz","l":"Superhero"}];updateSearchResults();
